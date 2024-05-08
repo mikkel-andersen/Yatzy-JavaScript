68176b53a7738b26c61c91d56b1d67264df21f53
Mikkel <112852611+mikkel-andersen@users.noreply.github.com>
2024-05-08 10:56:03 +0200

    Hej och välkommen till YatzyServer!

--- Backend/YatzyServer.mjs
+++ Backend/YatzyServer.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const app = express();
-const port = 7777;
+const port = 7766;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
