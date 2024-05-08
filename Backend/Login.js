732624d9d08320a2eac49e5d477712afd891be0f
BuildTools <unconfigured@null.spigotmc.org>
2024-05-08 11:04:19 +0200

    login

--- Backend/Login.js
+++ Backend/Login.js
@@ -1,27 +1,24 @@
-const Login = () => {
+function login() {
+    const username = document.getElementById('player-name').value;
 
-    const [username, setUsername] = useState('');
-
-    const handleLogin = async () => {
-        try{
-            const response = await fetch('http://localhost:3000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username})
-            });
-
-            const data = await response.json();
-            if(response.status === 200){
-                console.log('Login successful', data);
-                // Redirect to the game page
-            } else {
-                console.log('Login failed', data.message);
-            }
-        } catch (error) {
-            console.log('Login failed', error);
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(response.status === 200){
+            console.log('Login successful', data);
+            // Redirect to the game page
+        } else {
+            console.log('Login failed', data.message);
         }
-};
+    })
+    .catch(error => {
+        console.log('Login failed', error);
+    });
 }
 
